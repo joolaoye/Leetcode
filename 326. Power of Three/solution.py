6601f131070c44928e12719567e589b1dec3c008
PapiChuloXX <afrokid010404@gmail.com>
2023-09-05 15:16:21 -0500

    added Power of Three

---
+++ 326. Power of Three/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n < 1:
+            return False
+
+
+        while n % 3 == 0:
+           n /= 3
+
+        return n == 1
