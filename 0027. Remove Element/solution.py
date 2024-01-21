8d2ff24a8ce2923dbbd92a036404d1d60649b766
PapiChuloXX <afrokid010404@gmail.com>
2024-01-20 20:21:29 -0600

    Remove Element

---
+++ 0027. Remove Element/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            if nums[l] == val:
+                nums[l], nums[r] = nums[r], '-'
+                r -= 1
+            else:
+                l += 1
+
+        return l
